190a98ad5fd161ab762351c6d370e257832c1df1
i love femboys <ab37432@sersd.org>
2026-01-22 10:31:59 -0500

    Enhance main function with code generation and usage prompt
    Refactor main function to include code generation and improve error message.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,31 @@
 import sys
-import parser as parse
 from lexer import get_tokens
+import parser as parse
+from codegen import PythonCodeGen
 
 def main():
     if len(sys.argv) < 2:
-        print("yo no way it works???")
+        print("usage: python main.py <source_file>")
         sys.exit(1)
+
     with open(sys.argv[1], "r") as file:
         content = file.read()
+
+    # lexing stuffs
     tokens = get_tokens(content)
-    tokens.append(('EOF', 'EOF'))
-    parse.main(tokens)
+    tokens.append(('EOF', 'EOF', len(content)))
+
+    # parsing stuffs
+    p = parse.Parser(tokens)
+    ast = p.parse_program()
+
+    # codegen
+    cg = PythonCodeGen()
+    python_code = cg.generate(ast)
+
+    # codegen output
+    print("code generated (python)")
+    print(python_code)
 
 if __name__ == "__main__":
     main()
